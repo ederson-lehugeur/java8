e0deba2dcf6fe596dcd44004b6a7a0507b995d31
Éderson Lehugeur <eder.lehugeur@gmail.com>
2020-01-07 18:39:47 -0300

    Testes com Lambda

--- src/main/java/br/com/casadocodigo/java8/Capitulo2.java
+++ src/main/java/br/com/casadocodigo/java8/Capitulo2.java
@@ -5,9 +5,9 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class Capitulo2 {
-	
+
 	public static void main(String... args) {
-		
+
 		Usuario u1 = new Usuario("Paulo Silveira", 150);
 		Usuario u2 = new Usuario("Rodrigo Turini", 120);
 		Usuario u3 = new Usuario("Guilherme Silveira", 190);
@@ -17,16 +17,30 @@ public class Capitulo2 {
 		for (Usuario u : usuarios) {
 			System.out.println(u.getNome());
 		}
-		
+
+		// Java 8
+		Mostrador mostrador = new Mostrador();
+
+		usuarios.forEach(mostrador);
+
+		// Um pouco mais enxuto, mas ainda verboso.
 		usuarios.forEach(new Consumer<Usuario>() {
+			@Override
 			public void accept(Usuario p) {
 				System.out.println(p.getNome());
 			}
 		});
-		
+
+		Consumer<Usuario> mostrador2 = (Usuario u) -> { System.out.println(u.getNome()); };
+
+		Consumer<Usuario> mostrador3 = u -> { System.out.println(u.getNome()); };
+
+		Consumer<Usuario> mostrador4 = u -> System.out.println(u.getNome());
+
+		usuarios.forEach(mostrador4);
+
 		usuarios.forEach(u -> System.out.println(u.getNome()));
-		
+
 		usuarios.forEach(u -> u.tornaModerador());
-		
 	}
 }
---
+++ src/main/java/br/com/casadocodigo/java8/Mostrador.java
@@ -0,0 +1,12 @@
+package br.com.casadocodigo.java8;
+
+import java.util.function.Consumer;
+
+public class Mostrador implements Consumer<Usuario> {
+
+	@Override
+	public void accept(Usuario usuario) {
+		System.out.println(usuario.getNome());
+	}
+
+}
