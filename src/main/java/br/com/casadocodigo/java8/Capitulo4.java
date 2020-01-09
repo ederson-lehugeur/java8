c91a6dbffd35ced5848053e4d7ceb069ef08995d
Éderson Lehugeur <eder.lehugeur@gmail.com>
2020-01-09 15:20:54 -0300

    Default Methods

--- src/main/java/br/com/casadocodigo/java8/Capitulo4.java
+++ src/main/java/br/com/casadocodigo/java8/Capitulo4.java
@@ -1,41 +1,43 @@
 package br.com.casadocodigo.java8;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class Capitulo4 {
-	
+
 	public static void main(String... args) {
 
 		Usuario u1 = new Usuario("Paulo Silveira", 150);
 		Usuario u2 = new Usuario("Rodrigo Turini", 120);
 		Usuario u3 = new Usuario("Guilherme Silveira", 190);
-	
+
 		List<Usuario> usuarios = Arrays.asList(u1, u2, u3);
 
-		Consumer<Usuario> mostraMensagem = u -> 
-			System.out.println("antes de imprimir os nomes");
+		Consumer<Usuario> mostraMensagem = u -> System.out.println("antes de imprimir os nomes");
 
-		 Consumer<Usuario> imprimeNome = u -> 
-		 	System.out.println(u.getNome());
-		
- 		usuarios.forEach(mostraMensagem.andThen(imprimeNome));
+		Consumer<Usuario> imprimeNome = u -> System.out.println(u.getNome());
+
+		Consumer<Usuario> mostraSegundaMensagem = u -> System.out.println("depois de imprimir os nomes");
+
+		usuarios.forEach(mostraMensagem.andThen(imprimeNome).andThen(mostraSegundaMensagem));
 
 		List<Usuario> usuarios2 = new ArrayList<>();
 		usuarios2.addAll(usuarios);
 
- 		Predicate<Usuario> predicado = new Predicate<Usuario>() {
- 		 	public boolean test(Usuario u) {
- 		 		return u.getPontos() > 160;
- 		 	}
- 		 };
+		Predicate<Usuario> predicado = new Predicate<Usuario>() {
+			@Override
+			public boolean test(Usuario u) {
+				return u.getPontos() > 160;
+			}
+		};
 
- 		 usuarios2.removeIf(predicado);
- 		 usuarios2.forEach(u -> System.out.println(u));
+		usuarios2.removeIf(predicado);
+		usuarios2.forEach(u -> System.out.println(u));
 
- 		 usuarios2.removeIf(u -> u.getPontos() > 160);
+		usuarios2.removeIf(u -> u.getPontos() > 140);
+		usuarios2.forEach(u -> System.out.println(u));
 	}
 }
